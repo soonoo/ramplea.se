c4ba2258095a8824000bed42a250b821381f2b53
soonoo <qpseh2m7@gmail.com>
2019-06-13 22:20:52 +0900

    change promise to async

--- ramplease.js
+++ ramplease.js
@@ -1,4 +1,5 @@
 const axios = require('axios');
+const fs = require('fs');
 
 const mobile = `http://www.enuri.com/lsv2016/ajax/getListGoods_ajax.jsp\?random_seq=519
   &pageNum=1&pageGap=30&tabType=0&cate=070341&keyword=&brand=&factory=
@@ -12,17 +13,26 @@ const pc = `http://www.enuri.com/lsv2016/ajax/getListGoods_ajax.jsp?random_seq=7
 const urls = { mobile, pc };
 const capacities = [ '4G', '8G', '16G' ];
 
-for(const platform of Object.keys(urls)){
-  axios.get(urls[platform])
-    .then(({ data })=> {
-      const { groupModelList: priceList } = data.lpList.find(i => {
-        const model = i.strModelName;
-        return model.includes('삼성') && model.includes('21300');
-      });
+const main = async () => {
+  console.log('Fethcing DRAM prices ...');
+  const results = {};
 
-      console.log(platform);
-      for(const capacity of capacities.values()) {
-        console.log(`${capacity}:${priceList.find(i => i.strBeginnerDicCondiname === capacity).longMinprice}`);
-      }
-    })
+  for(const platform of Object.keys(urls)) {
+    const { data } = await axios.get(urls[platform]);
+    const { groupModelList: priceList } = data.lpList.find(i => {
+      const model = i.strModelName;
+      return model.includes('삼성') && model.includes('21300');
+    });
+    results[platform] = {};
+
+    for(const capacity of capacities.values()) {
+      const price = priceList.find(i => i.strBeginnerDicCondiname === capacity).longMinprice;
+      results[platform][capacity] = price;
+    }
+
+  }
+  console.log('Got DRAM prices!');
+  fs.writeFileSync('data.json', JSON.stringify(results))
 }
+
+main();
