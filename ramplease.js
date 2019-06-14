66a52dbdf21b7cb18718e5f301ccd56587e3a6fe
soonoo <qpseh2m7@gmail.com>
2019-06-15 01:17:59 +0900

    change the term 'pc' to 'desktop'

--- ramplease.js
+++ ramplease.js
@@ -8,20 +8,20 @@ const kr_laptop = `http://www.enuri.com/lsv2016/ajax/getListGoods_ajax.jsp?rando
   &pageNum=1&pageGap=30&tabType=0&cate=070341&keyword=&brand=&factory=
   &shop_code=&prtmodelno=&gb1=&gb2=&sponsorList=&infoAdList=&IsDeliverySumPrice=N
   &IsJungoPriceRemove=N&m_price=&start_price=&end_price='`;
-const kr_pc = `http://www.enuri.com/lsv2016/ajax/getListGoods_ajax.jsp?random_seq=721
+const kr_desktop = `http://www.enuri.com/lsv2016/ajax/getListGoods_ajax.jsp?random_seq=721
   &pageNum=1&pageGap=30&tabType=0&cate=070340&keyword=&brand=&factory=&shop_code=&prtmodelno=
   &gb1=&gb2=&sponsorList=&infoAdList=&IsDeliverySumPrice=N&IsJungoPriceRemove=N&m_price=
   &start_price=&end_price=`;
 const kr = {
   laptop: kr_laptop,
-  pc: kr_pc,
+  desktop: kr_desktop,
 };
 
 const us_laptop = `https://www.amazon.com/s?k=samsung+21300+SODIMM&rh=n%3A172500&ref=nb_sb_noss`;
-const us_pc = `https://www.amazon.com/s?k=samsung+21300+DIMM&rh=n%3A172500&ref=nb_sb_noss`;
+const us_desktop = `https://www.amazon.com/s?k=samsung+21300+DIMM&rh=n%3A172500&ref=nb_sb_noss`;
 const us = {
   laptop: us_laptop,
-  pc: us_pc,
+  desktop: us_desktop,
 };
 
 const capacities = [ '4G', '8G', '16G' ];
@@ -53,8 +53,7 @@ const main = async () => {
       const t = $(this).find('.a-size-medium').text().toLowerCase();
       const price = $(this).find('.a-offscreen').text().slice(1);
 
-      const term = platform === 'pc' ? 'desktop' : platform;
-      if(!t.includes(term)) return;
+      if(!t.includes(platform)) return;
 
       for(const capacity of capacities.map(c => c.toLowerCase()).values()) {
         const path = `us.${platform}.${capacity}`;
